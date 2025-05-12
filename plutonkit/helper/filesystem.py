34fc66bef6076eda3cea7c8580269dc954503909
pein <plants.coordinators@gmail.com>
2025-05-13 00:36:05 +0800

    Update pylintrc

--- plutonkit/helper/filesystem.py
+++ plutonkit/helper/filesystem.py
@@ -28,8 +28,8 @@ def create_yaml_file(project_name, filename, library=None):
         fw.write(yaml.dump(library, default_flow_style=False))
         fw.close()
 
-def write_file_content( # pylint: disable=too-many-arguments
-    directory: str, folder_name: str, file: str, content: str, args=None,block=None
+def write_file_content(
+    directory: str, folder_name: str, file: str, content: str, args=None,block=None # pylint: disable=too-many-arguments
 ):
     file_path = os.path.dirname(file)
     if file_path != "":

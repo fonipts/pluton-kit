34bb11f8e6636655ab824bc9bc57241c40e0280d
pein <plants.coordinators@gmail.com>
2025-09-10 03:10:26 +0800

    Update new revision

--- plutonkit/framework/template/TheTemplate.py
+++ plutonkit/framework/template/TheTemplate.py
@@ -12,6 +12,7 @@ from ..logic.ConditionSplit import ConditionSplit
 
 class TheTemplate:
     def __init__(self, content: str, args=None,block=None):
+        self.raw_content = content
         self.block = block
         self.args = args
         self.content = self.__wragle_data(content)
@@ -28,6 +29,10 @@ class TheTemplate:
         if param["type"] == "condition":
 
             return "", True
+
+        if param["type"] == "content":
+
+            return param["content"], True
 
         return "",raw_bool
 
@@ -57,11 +62,13 @@ class TheTemplate:
             return row_content
         return ""
 
-    def __get_cond_valid_data(self,append_data):
+    def __get_cond_valid_data(self,append_data,types:str):
         counter =0
         append_count = len(append_data)
         valid_cond = True
         valid_data = []
+        end_index = 0
+        start_index = 0
         while counter< append_count:
             row_data = append_data[counter]
             if row_data["type"] == "condition":
@@ -70,8 +77,20 @@ class TheTemplate:
                 cond = ConditionSplit(row_data["content"],self.args)
 
                 valid_cond = cond.validCond()
+                if types == "tag" and valid_cond:
+                    end_index = row_data["end_index"]
+                    start_index = row_data["start_index"]
 
             elif row_data["type"] == "end":
+                if types == "tag" and valid_cond:
+                    valid_data.append({
+                        "start_index":row_data["end_index"],
+                        "end_index":start_index,
+                        "raw":self.raw_content[start_index:row_data["end_index"]],
+                        "type":"content",
+                        "action":"content",
+                        "content":self.raw_content[end_index:row_data["start_index"]]
+                    })
                 valid_cond = True
             else:
                 if valid_cond:
@@ -96,7 +115,7 @@ class TheTemplate:
                 content = content.replace("".join(val), self.args.get(val[1], ""))
 
 
-        parse_tag = self.__get_cond_valid_data(parse.parse_tag)
+        parse_tag = self.__get_cond_valid_data(parse.parse_tag,"tag")
 
         for val in parse_tag:
             raw_content,raw_bool = self.__set_value_in_tags(val)
@@ -105,7 +124,7 @@ class TheTemplate:
 
         for val in parse.parse_block:
             sub_content = ""
-            append_row = self.__get_cond_valid_data(val["append"])
+            append_row = self.__get_cond_valid_data(val["append"],"block")
 
 
             for sv in append_row:
